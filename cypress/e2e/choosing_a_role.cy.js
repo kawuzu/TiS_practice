76dff906c3575fbb42e53af6bdbc8b9345f3f964
akiko <akikomoore8@gmail.com>
2025-05-14 02:48:19 +0700

    user_ch_a_role_fixed

--- cypress/e2e/choosing_a_role.cy.js
+++ cypress/e2e/choosing_a_role.cy.js
@@ -3,6 +3,22 @@ describe('Модуль Регистрации – выбор роли студе
         // Переход на сайт
         cy.visit('https://dev.profteam.su/');
 
+        // Нажатие на кнопку "Авторизация"
+        cy.get('#app > div.page > div > main > div > div.header-container__buttons > a:nth-child(1) > button')
+            .click();
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(1) > div > input')
+            .type('kawuzu');
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(2) > div.form-control--medium.form-control > input')
+            .type('emaepar0L');
+
+        // Нажатие кнопки "Войти"
+        cy.get('#app > div.page > div > section > form > div.form__buttons > div:nth-child(3) > button')
+            .click();
+
+        cy.url().should('not.include', '/login');
+
         // Нажатие на кнопку "Выбрать роль"
         cy.get('#app > div.page > div > div.page-navigation > div.page-nav > div.page-nav__menu-item > div.page-nav__role-block > button')
             .click();
